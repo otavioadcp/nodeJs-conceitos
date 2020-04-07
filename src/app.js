e10b136d185bdcab6f2e1f62447f69f2d3194a5e
Otavio <otavioadcp@gmail.com>
2020-04-07 13:19:04 -0400

    alterações no app.js para atender requisitos

--- src/app.js
+++ src/app.js
@@ -1,33 +1,82 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+let repositories = [{
+  "id": "fbce6a43-3998-4188-9cd3-d52fa44ccaca",
+  "title": "Otavio Augusto",
+  "url": "http://github.com/...",
+  "techs": [
+      "Node.js",
+      "React"
+  ],
+  "likes": 0
+}];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+
+  return response.status(200).json(repositories);
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  console.log('aq', request.body)
+  const {title, url, techs} = request.body;
+  
+  const newRepository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(newRepository);
+
+  return response.status(201).json(newRepository)
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  
+  const {id} = request.params;
+  const {title, url, techs} = request.body;
+
+  const index = repositories.findIndex(data => data.id === id)
+
+  title ? repositories[index].title = title : null
+  url ? repositories[index].url = url : null
+  techs ? repositories[index].techs = techs : null 
+
+  return response.status(200).json(repositories[index])
+
+
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const { id } = req.params;
+  
+  repositories = repositories.filter(data => data.id !== id);
+
+  return res.status(200).send()
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+    const { id } = request.params;
+
+    const index = repositories.findIndex(data => data.id === id);
+
+    repositories[index].likes = repositories[index].likes+1;
+
+    return response.status(200).json(repositories[index]);
+
 });
 
 module.exports = app;
